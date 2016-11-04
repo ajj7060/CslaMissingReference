6f623cfa35ea992c56121c353dd24392d704c037
ajj3085 <ajj3085@PLAGUE>
2016-11-04 16:06:47 -0400

    Add project files.

---
+++ Business/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Csla;
+
+namespace Business {
+	[Serializable]
+	public class Account : BusinessBase<Account> {
+		public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(x => x.Name);
+
+		public string Name {
+			get { return GetProperty(NameProperty); }
+			set { SetProperty(NameProperty, value); }
+		}
+
+		public static Account NewAccount() {
+			return DataPortal.Create<Account>();
+		}
+
+		protected override void DataPortal_Create() {
+			using (BypassPropertyChecks) {
+				Name = "name";
+			}
+		}
+	}
+}
